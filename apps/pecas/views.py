acc45903de5b36d64c4562800559ebd41aa32194
DESK01 <tiagorbv@yahoo.com.br>
2021-01-05 15:55:19 -0300

    lista de peças win

--- apps/pecas/views.py
+++ apps/pecas/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
-
+from .models import Pecas
+from .forms import PecasForm
 
 def ListaPecas(request):
-    object = {}
+    pecas = Pecas.objects.all().order_by("id")
+    object = {"pecas":pecas}
     return render(request, 'pecas/lista_pecas.html', object)
