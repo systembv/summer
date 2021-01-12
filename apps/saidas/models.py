807b26884a4fbf0102224af49098ceada2ef46fd
DESK01 <tiagorbv@yahoo.com.br>
2021-01-12 17:35:34 -0300

    ajuste app saidas desk

--- apps/saidas/models.py
+++ apps/saidas/models.py
@@ -3,10 +3,11 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from apps.pecas.models import Pecas
 from apps.unidades.models import Unidade
-
+from apps.funcionarios.models import Funcionarios
 
 class Saidas(models.Model):
     nome = models.CharField('Item de saida: ', max_length=80, help_text='Insira item de saida')
+    responsavel = models.ForeignKey(Funcionarios, on_delete=models.PROTECT, null=True, blank=True, help_text='Funcionário responsável do pedido', verbose_name="Responsável:")
     item = models.ForeignKey(Pecas, on_delete=models.PROTECT, null=True, blank=True, help_text='Insira o item de saida', verbose_name="Item de saida:")
     quantidade = models.DecimalField('Quantidade:', max_digits=10, decimal_places=2, blank=True, null=True, help_text='Quantidade item saida')
     unidade = models.ForeignKey(Unidade, on_delete=models.PROTECT, null=True, blank=True, help_text='Insira a unidade', verbose_name="Unidade:")
